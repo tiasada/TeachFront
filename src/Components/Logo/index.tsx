c65e5983fc1f54f68ca495fda18b16fd3a323ca4
guilhermekuehl <guikuehlkeller120@gmail.com>
2020-12-22 13:09:07 -0300

    Add login logo

---
+++ src/Components/Logo/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { StyledLogo } from './styles'
+
+const Logo = () => (
+    <StyledLogo src='../../Images/login.png'/>
+)
+export default Logo
---
+++ src/Components/Logo/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledLogo = styled.img`
+    position: absolute;
+    width: 201px;
+    height: 192px;
+    left: 620px;
+    top: 245px;
+`
