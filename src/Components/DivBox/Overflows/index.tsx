bcc2a2f3aa6da2a520f0aae7189174333afb4a7f
Yasmin Junckes <yjunckes@gmail.com>
2021-01-12 21:30:11 -0300

    using containers

--- src/Components/DivBox/Overflows/index.tsx
+++ src/Components/DivBox/Overflows/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { ClassOvFlow } from './styles'
+import { Container, Row} from 'react-bootstrap'
 
 const DivClasslist: React.FC = ({ children }) => (
-    <ClassOvFlow>{ children }</ClassOvFlow>
+    <Container ><Row className="justify-content-center">{ children }</Row></Container>
+       
 )
 
 export default DivClasslist
