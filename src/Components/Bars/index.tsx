7c8f8d3bdd0a03ec5ebf370bf85f933788332d4e
luanvitorfaustino <luanvitorfaustino.03@gmail.com>
2021-01-28 22:05:52 -0300

    adjusting themes

--- src/Components/Bars/index.tsx
+++ src/Components/Bars/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import SearchBarMatters from './styles'
+import { Colors, colors } from '/theme/colors'
 
-const Search = () => (
-    <SearchBarMatters placeholder="Search..." type="text" />
-)
+type Props = {
+    color: Colors
+  }
+
+const Search = ({color}: Props) => {
+    return( 
+    <SearchBarMatters placeholder="Search..." color={colors[color]} type="text" />
+    )
+}
 
 export default Search
