4763770214812912885ce9a0e8b26cbe5b6fa532
tiagosada <tiagocostasada@gmail.com>
2021-02-12 21:34:07 -0300

    adjusting /teacher, table

--- src/Ui/Tabs/styles.ts
+++ src/Ui/Tabs/styles.ts
@@ -1,26 +1,26 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { Nav, Navbar } from 'react-bootstrap'
 
 type Props = {
-    color: string
+  color: string
 }
 type TabProps = {
-    color: string
-    active?: boolean
+  color: string
+  active?: boolean
 }
 
-export const Navmenu = styled(Navbar)<Props>`
+export const Navmenu = styled(Navbar) <Props>`
     position: sticky;
     width: 100% ; 
     background-color: ${({ color }) => color};
 `
-export const NavClass = styled(Navmenu)<Props>`
+export const NavClass = styled(Navmenu) <Props>`
     background-image: none;
     background-color: ${({ color }) => color};
     display: flex;
     justify-content: space-between;
 `
-export const TabItem = styled(Nav.Item)<TabProps>`
+export const TabItem = styled(Nav.Item) <TabProps>`
     background-image: none;
     margin-right: 15px; 
     background-color: ${({ active, color }) => active ? 'rgba(180,180,180,0.3)' : color};
@@ -31,4 +31,20 @@ export const TabItem = styled(Nav.Item)<TabProps>`
     &:hover {
         background-color: #ddd;
     }
-    `
+`
+
+export const TabDiv = styled.div<Props>`
+  position: static;
+  width: 80%;
+  height: 500px;
+  top: 100px;
+  margin-left: auto;
+  margin-right: auto;
+  overflow: hidden;
+
+  background: ${({ color }) => color};
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-sizing: border-box;
+  
+`
